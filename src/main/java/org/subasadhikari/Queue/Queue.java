4e53619c237de2172456f511e37151f3e99b6895
subasAd <subasadhikari0025@gmail.com>
2023-11-23 17:04:12 +0545

    Initial queue class

---
+++ src/main/java/org/subasadhikari/Queue/Queue.java
@@ -0,0 +1,66 @@
+package org.subasadhikari.Queue;
+
+import java.util.Arrays;
+
+import static java.lang.Math.abs;
+
+public class Queue<T> {
+        private  int size;
+        private T[] queue;
+        private int curposFront ;
+        private int curposBack ;
+
+        public int getSize() {
+            return size;
+        }
+
+        public int getLength() {
+            return curposFront - curposBack;
+        }
+
+        public Queue(int size) {
+            this.size = size;
+            queue = (T[])new Object[this.size];
+            this.curposFront = 0;
+            this.curposBack = 0;
+
+        }
+        public boolean enqueue(T item){
+            if((curposFront +1)%this.size == curposBack%this.size ){
+                return false;
+            }else{
+                queue[(curposFront++)%this.size] = item;
+                return true;
+            }
+
+
+        }
+        public T dequeue() throws Exception {
+            if (curposFront%this.size==curposBack%this.size){
+                throw new Exception("Queue is empty");
+            }
+            T item  = queue[curposBack];
+            queue[(curposBack++)%this.size] = null;
+
+            return item;
+        }
+        public String toString()
+        {
+            return Arrays.toString(stackarray);
+        }
+        public T peek() throws Exception {
+            if(curposFront%this.size == curposBack%this.size)
+            {
+                throw new Exception("The Queue is empty");
+
+            }
+            return queue[(curposFront-1)%this.size];
+        }
+        public boolean isEmpty()
+        {
+            return curposFront%this.size == curposBack%this.size;
+        }
+        public boolean isFull() {return abs(curposFront%this.size-curposBack%this.size)==this.size;}
+    }
+
+
